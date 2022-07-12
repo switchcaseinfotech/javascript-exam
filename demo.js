3a3674522eed3b2b05a8abf1c0e73ab872ca3142
switchcaseinfotech <109139789+switchcaseinfotech@users.noreply.github.com>
2022-07-12 14:11:27 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,90 @@
+let arr = [{
+    id: 0,
+    name: "Nimesh Bhalani",
+    isActive: false,
+    isBlocked: false,
+    email: "nimesh@gmail.com",
+    password: "test@123"
+},{
+    id: 1,
+    name: "Milan B",
+    isActive: true,
+    isBlocked: false,
+    email: "milan@gmail.com",
+    password: "test@123"
+},{
+    id: 2,
+    name: "Parth G",
+    isActive: true,
+    isBlocked: true,
+    email: "parth@gmail.com",
+    password: "test@123"
+},{
+    id: 3,
+    name: "Vishal T",
+    isActive: false,
+    isBlocked: true,
+    email: "vishal@gmail.com",
+    password: "test@123"
+},{
+    id: 4,
+    name: "Test 1",
+    isActive: false,
+    isBlocked: true,
+    email: "test@gmail.com",
+    password: "test@123"
+},{
+    id: 5,
+    name: "Test 2",
+    isActive: true,
+    isBlocked: false,
+    email: "test2@gmail.com",
+    password: "test@123"
+}]
+
+let str = "The quick brown fox jumps over the lazy dog";
+
+let arr1 = [10, 11, 12, 9, 8, 13, 7]
+
+// use arr1 data 
+function findMaxNumber(){
+
+}
+
+// use arr1 data 
+function findMinNumber(){
+
+}
+
+// use arr data 
+function findActiveUser(){
+
+}
+
+// use arr data 
+function findBlockedUser(){
+
+}
+
+// use arr data 
+function findNonActiveAndBlockedUser(){
+
+}
+
+// // use arr data  & use reduce method here refer from https://www.w3schools.com/jsref/jsref_reduce.asp
+function findActiveUserCount(){
+ 
+}
+
+// // use arr data & use reduce method here refer from https://www.w3schools.com/jsref/jsref_reduce.asp
+function findBlockedUserCount(){
+
+}
+
+console.log("Max number: ---", findMaxNumber())
+console.log("Min number: ---", findMinNumber())
+console.log("Active users: ---", findActiveUser())
+console.log("Blocked users: ---", findBlockedUser())
+console.log("Non Active & Blocked user: ---", findNonActiveAndBlockedUser())
+console.log("Non active user count: ---", findActiveUserCount())
+console.log("Blocked user count: ---", findBlockedUserCount())
